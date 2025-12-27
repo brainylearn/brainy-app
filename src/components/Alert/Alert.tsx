76feb1afcca2037246c55f6e71d1aef7cd46dbf0
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-27 18:18:15 +0100

    style(alert): add secondary color

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -2,17 +2,24 @@ import { mdiCloseThick } from "@mdi/js";
 import styles from "./styles.module.css";
 import Icon from "@mdi/react";
 
-interface Props {
-	type: "error" | "primary";
-	className?: string;
+interface Props extends React.DetailedHTMLProps<
+	React.InputHTMLAttributes<HTMLInputElement>,
+	HTMLInputElement
+> {
+	type: "error" | "primary" | "secondary";
 	children: React.ReactNode;
 	onClose?: () => void;
 }
 
-function Alert({ className, type, children, onClose }: Props) {
+function Alert({ className, type, children, onClose, ...props }: Props) {
 	return (
 		<div
-			className={`${styles.alert} ${type === "error" ? styles.error : styles.primary} ${className}`}>
+			className={`${styles.alert}
+                ${type === "error" && styles.error}
+                ${type === "primary" && styles.primary}
+                ${type === "secondary" && styles.secondary}
+                ${className}`}
+			{...props}>
 			{children}
 			{onClose && (
 				<button type="button" onClick={onClose}>
