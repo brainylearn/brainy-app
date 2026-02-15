edb228db7d97872c53b65a95e23995b02945f2e5
Rami Alkawadri <ramialkawadri@hotmail.com>
2026-02-15 12:45:55 +0100

    chore: better logging

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -11,17 +11,19 @@ fn setup_sqlx() {
     let db_url = format!("sqlite:///{}?mode=rwc", db_path.display());
 
     let env_file = current_directory.join(".env");
+    println!("cargo:warning=.env file path is {}", env_file.display());
+
     if !env_file.exists() {
         let env_content = format!("DATABASE_URL=\"{0}\"", db_url);
         fs::write(&env_file, env_content).expect("Cannot write .env variable.");
-        println!("cargo:warning=✅ .env file created");
+        println!("cargo:warning=.env file created");
     } else {
-        println!("cargo:warning=✅ .env already exists");
+        println!("cargo:warning=.env already exists");
     }
 
     if db_path.exists() {
         fs::remove_file(&db_path).expect("Cannot remove database");
-        println!("cargo:warning=✅ removed previous temp.db file");
+        println!("cargo:warning=removed previous temp.db file");
     }
 
     let output = Command::new("sqlx")
@@ -44,7 +46,7 @@ fn setup_sqlx() {
         std::process::exit(1);
     } else {
         println!(
-            "cargo:warning=✅ migration is applied to {}",
+            "cargo:warning=migration is applied to {}",
             db_path.display()
         );
     }
