a5edb5581401066102295e68cd4e074faae6ef88
Rami Alkawadri <ramialkawadri@hotmail.com>
2026-03-20 23:54:15 +0100

    Hide database location for mobile

--- src/features/Settings/components/tabs/DataTab.tsx
+++ src/features/Settings/components/tabs/DataTab.tsx
@@ -1,10 +1,11 @@
 import { open } from "@tauri-apps/plugin-dialog";
-import { FormRows } from "../../../../components/Form/Form";
+import { FormRows, FormRowsProps } from "../../../../components/Form/Form";
 import CheckBox from "../../../../components/Checkbox/Checkbox";
 import Icon from "@mdi/react";
 import { mdiFolderOpenOutline } from "@mdi/js";
 import Settings from "../../../../types/backend/model/settings";
 import { TabProps } from "../../types/tabProps";
+import IsMobile from "../../../../utils/isMobile";
 
 export default function DataTab({ state, setState }: TabProps) {
 	const updateSettings = (newSettings: Partial<Settings>) => {
@@ -33,54 +34,53 @@ export default function DataTab({ state, setState }: TabProps) {
 		});
 	};
 
-	return (
-		state.localSettings && (
-			<FormRows
-				rows={[
-					{
-						label: "Database Location",
-						labelHtmlFor: "database-location",
-						children: (
-							<div className="row">
-								<input
-									id="database-location"
-									type="text"
-									value={state.localSettings.databaseLocation}
-									style={{ width: "100%" }}
-									readOnly
-									autoFocus
-								/>
-								<button
-									className="transparent"
-									type="button"
-									onClick={() =>
-										void handleChangeDatabaseLocationClick()
-									}>
-									<Icon
-										path={mdiFolderOpenOutline}
-										size={1}
-									/>
-								</button>
-							</div>
-						),
-					},
-					{
-						label: "Sync on startup and on close",
-						labelHtmlFor: "auto-sync",
-						children: (
-							<CheckBox
-								id="auto-sync"
-								checked={state.localSettings.autoSync}
-								onChange={e =>
-									updateSettings({
-										autoSync: e.target.checked,
-									})
-								}
-							/>
-						),
-					},
-				]}
-			/>
-		)
-	);
+	const formRowsProps: FormRowsProps = {
+		rows: [
+			{
+				label: "Sync on startup and on close",
+				labelHtmlFor: "auto-sync",
+				children: (
+					<CheckBox
+						id="auto-sync"
+						checked={state.localSettings?.autoSync}
+						onChange={e =>
+							updateSettings({
+								autoSync: e.target.checked,
+							})
+						}
+					/>
+				),
+			},
+		],
+	};
+
+	// TODO: unit test
+	if (!IsMobile()) {
+		formRowsProps.rows.push({
+			label: "Database Location",
+			labelHtmlFor: "database-location",
+			children: (
+				<div className="row">
+					<input
+						id="database-location"
+						type="text"
+						value={state.localSettings?.databaseLocation}
+						style={{ width: "100%" }}
+						readOnly
+						autoFocus
+					/>
+					<button
+						className="transparent"
+						type="button"
+						onClick={() =>
+							void handleChangeDatabaseLocationClick()
+						}>
+						<Icon path={mdiFolderOpenOutline} size={1} />
+					</button>
+				</div>
+			),
+		});
+	}
+
+	return state.localSettings && <FormRows {...formRowsProps} />;
 }
