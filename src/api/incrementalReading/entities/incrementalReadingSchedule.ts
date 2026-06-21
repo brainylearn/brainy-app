e6682da43471edf285707251e6b6a74a0e3dbeb4
Rami Alkawadri <ramialkawadri@hotmail.com>
2026-06-21 22:03:04 +0200

    Add rest of fields

--- src/api/incrementalReading/entities/incrementalReadingSchedule.ts
+++ src/api/incrementalReading/entities/incrementalReadingSchedule.ts
@@ -1,4 +1,13 @@
+import { IncrementalReadingPriority } from "../../cells/valueObjects/incrementalReading";
+
 export default interface IncrementalReadingSchedule {
+	id: string;
+	createdDate: string;
+	modifiedDate: string;
+	cellId: string;
+	priority: IncrementalReadingPriority;
+	title: string;
 	nextReadingDate: string;
 	completed: boolean;
+	hasExtracts: boolean;
 }
